11f88cba59ed11d8e66f486ea5fbcf3436f729e5
Marek Dziewit <marek2901@gmail.com>
2018-08-09 00:43:19 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,29 @@
+# Fibonacci implementation
+
+This project is an attempt to optimize regular recursion call with stack usage with trampolines (TRAMPOLINING is a functional programming concnept allowing to transform stack based recursion to iteration)
+
+> Take a look at main.rb for implementation details
+
+## Getting Started
+
+`git clone` cd into project and `bundle install`
+
+## Running the tests
+
+run `ruby main.rb` in project directory
+
+## Contributing
+
+Feel free to write PR or Open Issues in case of any questions
+
+## Authors
+
+* **Marek Dziewit**
+
+## License
+
+This project is licensed under the MIT License
+
+## Acknowledgments
+
+* Thanks [Paweł](https://github.com/pawel-gawel) and [Michał](https://github.com/mpanczyk) for Inspiration
---
+++ main.rb
@@ -0,0 +1,54 @@
+# normal useless way
+
+def fib(num)
+  return 0 if num.zero?
+  return 1 if num == 1
+
+  fib(num - 1) + fib(num - 2)
+end
+
+# tail call optimization
+
+def inner_fib(num, a, b)
+  return a if num.zero?
+  return b if num == 1
+
+  inner_fib(num - 1, b, a + b)
+end
+
+def fib_tail(num)
+  inner_fib(num, 0, 1)
+end
+
+# TRAMPOLINES
+
+DONE_PROC = proc do |num|
+  { done: true,
+    value: num }
+end
+
+CONTINUE_PROC = proc do |func|
+  { func: func,
+    done: false }
+end
+
+def inner_fib_tramp(num, a, b)
+  return DONE_PROC.call(a) if num.zero?
+  return DONE_PROC.call(b) if num == 1
+
+  CONTINUE_PROC.call(proc { inner_fib_tramp(num - 1, b, a + b) })
+end
+
+def trampoline_it(func)
+  result = func.call
+  result = result[:func].call until result[:done]
+  result[:value]
+end
+
+def fib_tramp(num)
+  trampoline_it(proc { inner_fib_tramp(num, 0, 1) })
+end
+
+num = ARGV.shift.to_i || 1_000_000
+
+puts "fib #{num} eq #{fib_tramp(num)}"
