c7cbecf3e004724e31607309a2e2a9dd3340a5a3
Michał Pańczyk <mjpanczyk@gmail.com>
2018-08-30 10:14:22 +0200

    Fix: use `return` instead of `exit`

--- bash/main.sh
+++ bash/main.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 fib_tramp() {
-  [ $1 == 0 ] && echo $2 && exit 0
+  [ $1 == 0 ] && echo $2 && return 0
   COUNTER=$((${1}-1))
   A=$3
   B=$(echo ${2}+${3} | bc | tr -d '\\' | tr -d $'\n')
@@ -10,11 +10,11 @@ fib_tramp() {
 
 trampoline() {
   RESULT="$($@)"
-  grep -Eq '^[0-9]+$' <(echo $RESULT) && echo $RESULT && exit 0
+  grep -Eq '^[0-9]+$' <(echo $RESULT) && echo $RESULT && return 0
   while true
     do
       RESULT="$(eval $RESULT)"
-      grep -Eq '^[0-9]+$' <(echo $RESULT) && echo $RESULT && exit 0
+      grep -Eq '^[0-9]+$' <(echo $RESULT) && echo $RESULT && return 0
     done
 }
 
